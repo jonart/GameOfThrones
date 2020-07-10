972d6777e04312b28c1c8cb9b9e553ef105fe462
kamishnikov@mixcart.ru <Cherry1018>
2020-07-10 16:08:06 +0300

    Add SplashScreen

---
+++ app/src/main/java/ru/skillbranch/CharacterScreen.kt
@@ -0,0 +1,4 @@
+package ru.skillbranch
+
+class CharacterScreen {
+}
---
+++ app/src/main/java/ru/skillbranch/gameofthrones/utils/Api.kt
@@ -0,0 +1,10 @@
+package ru.skillbranch.gameofthrones.utils
+
+import io.reactivex.Single
+import retrofit2.http.GET
+import ru.skillbranch.gameofthrones.data.remote.res.HouseRes
+
+interface Api {
+    @GET("/houses")
+    fun getHouses(): Single<HouseRes>
+}
